5cd4d7f299e65fb34b90251597b8bb3d3d1dac0e
Stephen Aranda <aranda.stephen88@gmail.com>
2022-09-14 11:38:47 -0500

    added includes for iostream and math.h

--- math_ops.h
+++ math_ops.h
@@ -4,3 +4,10 @@
  * File   : math_ops.h
  * Description : prototypes and definitions of mathematical operations for physics class
 */
+
+
+#include <iostream>
+#include <math.h>
+
+
+// * Consts for the math operations of the functions.
