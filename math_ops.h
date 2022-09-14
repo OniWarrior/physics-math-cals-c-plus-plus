e6f4f016ea1d58ba7cf881f78976e8ed80f0b608
Stephen Aranda <aranda.stephen88@gmail.com>
2022-09-14 12:45:40 -0500

    added blank block of calculation of standard error of mean

--- math_ops.h
+++ math_ops.h
@@ -24,6 +24,9 @@ double calculateMean(){
 
 // * Function    -- calculateStandardDev
 // * Description -- calculate the standard deviation of a data set
+// * lower-case sigma = Square root [Sigma((m-i)-(m-ave))^2 / (n-1)]
+// * m-i is the individual measurement in a trial and m-ave is the average of all measurements
+// * from all trials that were conducted in the experiment.
 double calculateStandardDev(){
 
 }
@@ -32,6 +35,7 @@ double calculateStandardDev(){
 // * Function    -- calculateBestFitLineSlope
 // * Description -- calculate the slope of the best fit line
 // * m = sigma ([x-i]-[x-ave])([y-i]-[y-ave]) / sigma ([x-i]-[x-ave]) ^ 2
+// * m represents the slope of the best fit line.
 double calculateBestFitLineSlope(){
 
 }
@@ -66,5 +70,16 @@ double calcualteSigmaTotalSquares(){
 // * Description -- Calculate the coefficient of determination
 // * R ^ 2 = 1 - (SS-res/SS-tot)
 double calculateCoeffOfDet(){
+
+}
+
+
+// * Function    -- calculateSEOM
+// * Description -- calculate the standard error of the mean
+// * lower-case sigma-sub-m-ave = standard deviation(lower-case-sigma) / (square-root(n))
+// * n represents the number of measurements.
+double calculateSEOM(){
     
 }
+
+
